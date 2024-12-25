554c94f5690179af99b0fb8fe47ea02ec23d3907
DESKTOP-EOPH6MV\mascot <dmstn1812@naver.com>
2024-12-25 22:27:56 +0900

    2d 학습까지 작성

--- src/pages/superviserd-learning.tsx
+++ src/pages/superviserd-learning.tsx
@@ -4,6 +4,7 @@ import * as tf from '@tensorflow/tfjs';
 import { Typography } from '@mui/material';
 import { useEffect } from 'react';
 import * as tfvis from '@tensorflow/tfjs-vis'
+import { ModelFitArgs, Rank, Sequential, Tensor } from '@tensorflow/tfjs';
 
 type CarDataType = {
   Name: string,
@@ -20,6 +21,15 @@ type CarDataType = {
 type FilteredCarDataType = {
   mpg: number,
   horsepower: number
+}
+
+type TrainingDataType = {
+  inputs: number[],
+  labels: number[],
+  inputMax: number,
+  inputMin: number,
+  labelMax: number,
+  labelMin: number
 }
 
 export default function SuperviserdLearning() {
@@ -44,7 +54,7 @@ export default function SuperviserdLearning() {
   /**
    * 모델 생성
    * */
-  const createModel = () => {
+  const createModel = (): Sequential => {
     // 순차 모델 인스턴스화
     const model = tf.sequential();
     // Add a single input layer
@@ -53,32 +63,6 @@ export default function SuperviserdLearning() {
     model.add(tf.layers.dense({ units: 1, useBias: true }));
 
     return model;
-  }
-
-  /**
-   * tfjs-vis 라이브러리로 데이터 시각화
-   * */
-  const renderVis = async () => {
-    const data = await getData();
-    console.log('data: ', data);
-    const values = data.map(d => ({
-      x: d.horsepower,
-      y: d.mpg,
-    }));
-
-    // model 생성
-    const model = createModel();
-    tfvis.show.modelSummary({ name: 'Model Summary' }, model);
-    tfvis.render.scatterplot(
-      {name: 'Horsepower v MPG'},
-      {values},
-      {
-        xLabel: 'Horsepower',
-        yLabel: 'MPG',
-        height: 300
-      }
-    );
-    // More code will be added below
   }
 
   /**
@@ -92,27 +76,18 @@ export default function SuperviserdLearning() {
     // intermediate tensors.
 
     return tf.tidy(() => {
-      // Step 1. Shuffle the data
-      /**
-       * 여기서는 학습 알고리즘에 제공할 예시의 순서를 무작위로 지정합니다.
-       * 일반적으로 학습하는 동안 데이터 세트는 모델이 학습할 크기가 작은 하위 집합(배치라고 함)으로 분할되기 때문에 셔플이 중요합니다.
-       * 셔플은 각 배치에 전체 데이터 분포의 데이터가 다양하게 포함되도록 하는 데 도움이 됩니다.
-       * 데이터가 다양하게 포함되도록 하면 모델에 다음과 같은 이점이 있습니다.
-       *
-       * 제공된 데이터의 순서에만 의존하여 학습하지 않도록 합니다.
-       * 하위 그룹의 구조에 민감해지지 않도록 합니다.
-       * 예를 들어 학습의 전반부에만 높은 마력의 자동차가 있다면 나머지 데이터 세트에는 적용되지 않는 관계를 학습할 수 있습니다.
-       * */
+      // Step 1. 데이터 셔플링
       tf.util.shuffle(data);
 
-      // Step 2. Convert data to Tensor
+      // Step 2. 데이터 텐서로 변환
       const inputs = data.map(d => d.horsepower)
       const labels = data.map(d => d.mpg);
 
-      const inputTensor = tf. (inputs, [inputs.length, 1]);
+      const inputTensor = tf.tensor2d(inputs, [inputs.length, 1]);
       const labelTensor = tf.tensor2d(labels, [labels.length, 1]);
 
-      //Step 3. Normalize the data to the range 0 - 1 using min-max scaling
+      //Step 3. 테이터 정규화
+      // min-max scaling 을 사용하여 데이터 숫자 범위를 0 - 1 사이로 조정함
       const inputMax = inputTensor.max();
       const inputMin = inputTensor.min();
       const labelMax = labelTensor.max();
@@ -131,6 +106,61 @@ export default function SuperviserdLearning() {
         labelMin,
       }
     });
+  }
+
+  const trainModel = async (model: Sequential, inputs: Tensor<Rank>, labels: Tensor<Rank>) => {
+    // Prepare the model for training.
+    model.compile({
+      optimizer: tf.train.adam(),
+      loss: tf.losses.meanSquaredError,
+      metrics: ['mse']
+    });
+
+    const batchSize = 32;
+    const epochs = 50;
+    const args: any = {
+      batchSize,
+      epochs,
+      shuffle: true,
+      callbacks: tfvis.show.fitCallbacks(
+        { name: 'Training Performance' },
+        ['loss', 'mse'],
+        { height: 200, callbacks: ['onEpochEnd'] }
+      )
+    };
+
+    return await model.fit(inputs, labels, args);
+  }
+
+  /**
+   * tfjs-vis 라이브러리로 데이터 시각화
+   * */
+  const renderVis = async () => {
+    const data = await getData();
+    console.log('data: ', data);
+    const values = data.map(d => ({
+      x: d.horsepower,
+      y: d.mpg,
+    }));
+
+    // model 생성
+    const model:Sequential = createModel();
+
+    tfvis.show.modelSummary({ name: 'Model Summary' }, model);
+    tfvis.render.scatterplot(
+      {name: 'Horsepower v MPG'},
+      {values},
+      {
+        xLabel: 'Horsepower',
+        yLabel: 'MPG',
+        height: 300
+      }
+    );
+    // 데이터 변환
+    const tensorData = convertToTensor(data);
+    const { inputs, labels } = tensorData;
+    trainModel(model, inputs, labels);
+    console.log('Done Training');
   }
 
   useEffect(() => {
